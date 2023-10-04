dcc87ef3dc406b250da691e4519925d25840908a
John <johnfarris217@gmail.com>
2023-10-03 21:24:28 -0500

    commit tree 99fedfffb948933381c36974937f3564fbabdca6

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,33 @@
 class ItemsController < ApplicationController
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "Item not found" }, status: :not_found
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "Item not found" }, status: :not_found
+  end
+
+  def create
+    item = Item.create(item_params)
+    render json: item, status: :created
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price, :user_id)
   end
 
 end
